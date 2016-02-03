73281e07d1751cdce9ac1be3c3a9ad956cfdf8b4
Pieter Provoost <pieterprovoost@gmail.com>
2016-02-03 12:09:52 +0100

    cleanup

---
+++ gbif_datasets.sql
@@ -0,0 +1,22 @@
+﻿drop table if exists gbif.datasets;
+
+create table gbif.datasets as
+select
+	datasetkey,
+	sum(records) as records,
+	cast(null as text) as publishingorganizationkey,
+	cast(null as text) as doi,
+	cast(null as text) as type,
+	cast(null as text) as title,
+	cast(null as text) as description,
+	cast(null as timestamp) as pubdate,
+	cast(null as text) as taxonomiccoverage,
+	cast(null as text) as geographiccoverage
+from gbif.occurrence
+inner join gbif.names
+on occurrence.taxonkey = names.taxonkey
+where names.worms_marine = true or names.irmng_marine = true
+group by datasetkey
+order by datasetkey;
+
+create index datasets_ix_datasetkey on gbif.datasets using btree(datasetkey);
